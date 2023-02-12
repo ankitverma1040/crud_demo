351c3d9e46c5a50aadb8a52d4a604658c3609b8a
ankitverma1040 <ankitverma1040@gmail.com>
2023-02-12 23:33:30 +0530

    again modified

--- src/main/java/com/raj/controller/StudentController.java
+++ src/main/java/com/raj/controller/StudentController.java
@@ -6,10 +6,7 @@ import com.raj.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/my")
@@ -23,6 +20,11 @@ public class StudentController {
     public ResponseEntity<Student> createStudentData(@RequestBody StudentDto studentDto){
         Student response = studentService.createNewStudent(studentDto);
         return  new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public void getStudent(){
+
     }
 
 }
